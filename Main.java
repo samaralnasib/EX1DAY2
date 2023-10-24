390d10c5638390d79a90f954e74c849957f3c7b0
samaralnasib <148685912+samaralnasib@users.noreply.github.com>
2023-10-24 09:48:37 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,82 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        //Q1
+
+//Scanner input = new Scanner(System.in);
+//System.out.println("Please inter your weight");
+//double yourWeight = input.nextDouble();
+//System.out.println("Please inter your height");
+//double yourHeight = input.nextDouble();
+//System.out.println("The BMI is" + (yourWeight / (yourHeight * yourHeight)));
+
+
+//Q2
+//Scanner input = new Scanner(System.in);
+//System.out.println("Marks");
+//int mark = input.nextInt();
+//float percentage = ((mark/100.0f)*100);
+//System.out.println("percentage : ");
+//System.out.println(percentage);
+
+//Q3
+//Scanner m = new Scanner(System.in);
+//System.out.println(" the amount in USD");
+//double amount = m.nextDouble();
+//double exchangeRate = 0.85;
+//double trate = amount * exchangeRate;
+//DecimalFormat decimalF = new DecimalFormat("#.00");
+//String formattedAmount = decimalF.format(trate);
+//System.out.println("Amount in EUR" + formattedAmount);
+
+//Q4
+//Scanner s = new Scanner(System.in);
+//System.out.println("Enter the word ");
+//String theword = s.nextLine();
+//StringBuilder reversed = new StringBuilder(theword);
+//reversed.reverse();
+//int length = theword.length();
+//System.out.println("Length: " + length + ("Reversed: " + reversed));
+
+//Q5
+//Scanner input = new Scanner(System.in);
+//System.out.println("Sentence");
+//String word = input.nextLine();
+//System.out.println("start index");
+//int sIndex = input.nextInt();
+//System.out.println("end index");
+//int eIndex = input.nextInt();
+//String str = word.substring(sIndex,eIndex);
+//System.out.println("Sentence atter extract substring:");
+//System.out.println(str);
+
+//Q6
+//Scanner input = new Scanner(System.in);
+//System.out.println("Sentence");
+//String s = input.nextLine();
+//String a = ("jumps");
+//boolean result = Boolean.parseBoolean((a));
+//System.out.println(result);
+
+//Q7
+//Scanner input = new Scanner(System.in);
+//System.out.println("sentence");
+//String s = input.nextLine();
+//System.out.println("Enter the word");
+//String word1 = input.nextLine();
+//System.out.println("Enter the word");
+//String word2 = input.nextLine();
+//String after = s.replace(word1,word2);
+//System.out.println(after);
+
+//Q8
+//Scanner input = new Scanner(System.in);
+//System.out.println("string 1");
+//String string1 = input.nextLine();
+//System.out.println("string 2");
+//String string2 = input.nextLine();
+//System.out.println(string1 == string2 );
+
+    }
+}
